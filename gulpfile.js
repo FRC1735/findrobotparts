a47b36f5d98b53b0657cea045051ea0c34a59bda
Nick Galotti <ngalotti@gmail.com>
2021-05-30 21:30:15 -0400

    FRP-8 Created build to create sitemap.xml Added to build to add the category images

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 const gulp = require("gulp");
-var handlebars = require('gulp-handlebars');
-var wrap = require('gulp-wrap');
-var declare = require('gulp-declare');
-var concat = require('gulp-concat');
-var del = require("del");
+const handlebars = require('gulp-handlebars');
+const wrap = require('gulp-wrap');
+const declare = require('gulp-declare');
+const concat = require('gulp-concat');
+const del = require("del");
+const jsonTransform = require('gulp-json-transform');
+const rename = require("gulp-rename");
 
-function clean() {
+assets = () => {
+	return gulp.src("./src/images/categories/*")
+	.pipe(gulp.dest("./build/images/categories"));
+}
+
+buildsitemap = () => {
+	return gulp.src('./src/categories.json')
+		.pipe(jsonTransform(function (data, file) {
+			let output = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+			output += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`
+			output += `\t<url><loc>http://findrobotparts.com/</loc></url>\n`;
+			data.groups.forEach(element => {
+				element.categories.forEach(element => {
+					output += `\t<url><loc>http://findrobotparts.com/${element.path}</loc></url>\n`
+				});
+			});
+			output += `</urlset>`;
+			return output;
+		}))
+		.pipe(rename("sitemap.xml"))
+		.pipe(gulp.dest('./build'));
+}
+
+clean = () => {
 	return del(['./build'])
 }
 
-const build = gulp.series(clean)
+const build = gulp.series(clean, buildsitemap, assets)
 
 exports.default = build;
