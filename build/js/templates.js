49befd3b45e2b7e0351cb75e97b7c2b2d80c26c9
Nick Galotti <ngalotti@gmail.com>
2021-05-31 22:50:34 -0400

    FRP-3 robotparts.css - Added color options for list-group - Cleaned up css for table-collapse
    productrow.hbs
    - Changed first column to be th
    - Changed links to list-group
    
    productrowheader.hbs
    - Added scope to th

--- build/js/templates.js
+++ build/js/templates.js
@@ -21,9 +21,9 @@ this["FindRobotParts"]["templates"]["productrow"] = Handlebars.template({"1":fun
         return undefined
     };
 
-  return "		<a class=\"btn btn-sm\" href=\""
+  return "			<a href=\""
     + alias2(alias1((depth0 != null ? lookupProperty(depth0,"link") : depth0), depth0))
-    + "\" role=\"button\" target=\"_blank\">"
+    + "\" class=\"list-group-item list-group-item-action list-group-item-green\" target=\"_blank\">"
     + alias2(alias1((depth0 != null ? lookupProperty(depth0,"vendor") : depth0), depth0))
     + "</a>\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
@@ -34,17 +34,17 @@ this["FindRobotParts"]["templates"]["productrow"] = Handlebars.template({"1":fun
         return undefined
     };
 
-  return "<tr>\n	<td data-label=\"Product\">\n		<strong>"
+  return "<tr>\n	<th scope=\"row\" data-label=\"Product\">\n		<strong>"
     + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":3,"column":10},"end":{"line":3,"column":18}}}) : helper)))
     + "</strong><br>\n		<img src=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"image") || (depth0 != null ? lookupProperty(depth0,"image") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data,"loc":{"start":{"line":4,"column":12},"end":{"line":4,"column":21}}}) : helper)))
     + "\" width=\"100\" alt=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":4,"column":40},"end":{"line":4,"column":48}}}) : helper)))
-    + "\">\n	</td>\n"
+    + "\">\n	</th>\n"
     + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"categories") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":1},"end":{"line":8,"column":10}}})) != null ? stack1 : "")
-    + "	<td data-label=\"Vendors\">\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"links") : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":10,"column":2},"end":{"line":12,"column":11}}})) != null ? stack1 : "")
-    + "	</td>\n</tr>\n";
+    + "	<td data-label=\"Vendors\">\n		<div class=\"list-group\">\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(alias1,(depth0 != null ? lookupProperty(depth0,"links") : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":11,"column":3},"end":{"line":13,"column":12}}})) != null ? stack1 : "")
+    + "		</div>\n	</td>\n</tr>\n";
 },"useData":true});
 this["FindRobotParts"]["templates"]["productrowheader"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     return "		<th>"
@@ -58,7 +58,7 @@ this["FindRobotParts"]["templates"]["productrowheader"] = Handlebars.template({"
         return undefined
     };
 
-  return "<thead>\n	<tr>\n		<th>Product</td>\n"
+  return "<thead>\n	<tr>\n		<th scope=\"col\">Product</td>\n"
     + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"categories") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":4,"column":2},"end":{"line":6,"column":11}}})) != null ? stack1 : "")
     + "	</tr>\n</thead>\n";
 },"useData":true});
