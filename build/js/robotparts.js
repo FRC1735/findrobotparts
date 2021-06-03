9c050d8c5ad14fc79d1e3720eaa6d5282191c75e
Nick Galotti <ngalotti@gmail.com>
2021-06-03 07:55:43 -0400

    FRP-5 product.html - updated styling of show all
    robotparts.js
    - swapped total and shown
    - added update shown when tags are clicked
    - removed old commented out code

--- build/js/robotparts.js
+++ build/js/robotparts.js
@@ -74,8 +74,8 @@ addShowMore = () => {
 }
 
 updateNumberShown = () => {
-	const total = document.querySelectorAll('#productdata tbody tr:not(.d-none)').length;
-	const shown = document.querySelectorAll('#productdata tbody tr').length;
+	const shown = document.querySelectorAll('#productdata tbody tr:not(.d-none)').length;
+	const total = document.querySelectorAll('#productdata tbody tr').length;
 	document.getElementById('numShown').innerHTML = shown;
 	document.getElementById('numTotal').innerHTML = total;
 	if (total > shown) {
@@ -117,70 +117,9 @@ document.getElementById('sidebar').addEventListener('change', () => {
 		}
 	
 	});
+	updateNumberShown();
 });
 
 document.addEventListener('DOMContentLoaded', () => {
 	requestData();
 });
-
-
-
-
-
-
-/*
-$(document).ready( function() {
-
-	if($(".table-collapse").length) {
-		$('.table-collapse td').each(function(){
-			var th = $(this).closest('table').find('th').eq( this.cellIndex ).text();
-			$(this).attr('data-label', th).html();
-		});
-	}
-
-	if( $("#sidebar").text().trim() == "") { 
-		$("#filterbutton").hide() 
-	}
-
-	storedExpandData = sessionStorage.getItem("expand");
-	currentPage = window.location.pathname.split("/")[1];
-	if (storedExpandData != null && storedExpandData.length > 0) { 
-		storedExpandData = JSON.parse(storedExpandData)
-		if (!(currentPage in storedExpandData)) {
-			storedExpandData[currentPage] = [];
-		} 
-	} else {
-		storedExpandData = {};
-		storedExpandData[currentPage] = [];
-	}
-
-	$("body").on("click", ".collapsetitle", function() {
-		var clickedexpander = $(this);
-		index = clickedexpander.data("index");
-		if(clickedexpander.hasClass("collapsed")) { // collapsing
-			if($.inArray(index, storedExpandData[currentPage]) < 0) {
-				storedExpandData[currentPage].push(index);
-				storeExpandData();
-			}
-		} else { //expanding
-			storedExpandData[currentPage].splice($.inArray(index, storedExpandData[currentPage]), 1);
-			storeExpandData();
-		}
-	
-	});
-	
-	$(".collapsetitle").each(function(index) {
-		var expander = $(this);
-		expander.data("index", index);	
-		if ( $.inArray(index, storedExpandData[currentPage]) > -1 ) {
-			expander.click();
-		}
-	});
-	
-	function storeExpandData() {
-		sessionStorage.expand = JSON.stringify(storedExpandData);
-	}
-
-  $('[data-toggle="tooltip"]').tooltip()
-
-});*/
