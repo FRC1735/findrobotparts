3898091f184497edfd43502a278182bcf6c0852c
Nick Galotti <ngalotti@gmail.com>
2021-10-30 13:23:25 -0400

    FRP-22 admin.py - Added check for dryrun
    template.html
    - Changed menu name

--- build/bin/admin.py
+++ build/bin/admin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import MySQLdb as mdb
+import MySQLdb
 import sys
 import json
 import cgitb
@@ -16,9 +16,9 @@ cgitb.enable()
 
 #create database connection
 try:
-	conn = mdb.connect(host=config.sqlh, db=config.sqld, passwd=config.sqlp, user=config.sqlu)
-	cursor = conn.cursor(mdb.cursors.DictCursor)
-except mdb.Error, e:
+	conn = MySQLdb.connect(host=config.sqlh, db=config.sqld, passwd=config.sqlp, user=config.sqlu)
+	cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+except MySQLdb.Error, e:
 	print("Content-type: text/html\n\n")
 	print("<h2>Error</h2>")
 	print("Error %d: %s" % (e.args[0], e.args[1]))
@@ -46,8 +46,11 @@ try:
 		if data["type"] == "addSingle" :
 			sql = "INSERT INTO products (name, image) VALUES(%s, %s)" % (conn.literal(data["name"]), conn.literal(data["image"]))
 			sqlcommands.append(sql)
-			cursor.execute(sql)
-			newid = cursor.lastrowid
+			if (data["dryrun"]) :
+				newid = "123"
+			else :
+				cursor.execute(sql)
+				newid = cursor.lastrowid
 			
 			sql = ""
 			for link in data["links"] :
@@ -56,7 +59,8 @@ try:
 				sql += "(%s, %s, %s)" % (conn.literal(newid), conn.literal(link["name"]), conn.literal(link["link"]))
 			sql = "INSERT INTO links (productid, vendor, link) VALUES %s" % sql
 			sqlcommands.append(sql)
-			cursor.execute(sql)
+			if (not data["dryrun"]) :
+				cursor.execute(sql)
 
 			sql = ""
 			for tag in data["tags"] :
@@ -66,20 +70,22 @@ try:
 			sql = "INSERT INTO producttag (productid, tagid) VALUES %s" % sql
 			sqlcommands.append(sql)
 			cursor.execute(sql)
-			if (data['dryrun'] == False) :
+			if (data['dryrun']) :
+				outputhtml = 'Dry run'
+			else :
 				#conn.commit()
 				outputhtml = 'Successful'
-			else :
-				outputhtml = sqlcommands
 		elif data["type"] == "edit" :
 			productid = data["productid"]
 			sql = "UPDATE products SET name=%s, image=%s WHERE productid=%s" % (conn.literal(data["name"]), conn.literal(data["image"]), conn.literal(productid))
 			sqlcommands.append(sql)
-			cursor.execute(sql)
+			if (not data["dryrun"]):
+				cursor.execute(sql)
 			
 			sql = "DELETE FROM links WHERE productid=%s" % conn.literal(productid)
 			sqlcommands.append(sql)
-			cursor.execute(sql)
+			if (not data["dryrun"]) :
+				cursor.execute(sql)
 
 			sql = ""
 			for link in data["links"] :
@@ -88,11 +94,13 @@ try:
 				sql += "(%s, %s, %s)" % (conn.literal(productid), conn.literal(link["name"]), conn.literal(link["link"]))
 			sql = "INSERT INTO links (productid, vendor, link) VALUES %s" % sql
 			sqlcommands.append(sql)
-			cursor.execute(sql)
+			if (not data["dryrun"]) :
+				cursor.execute(sql)
 
 			sql = "DELETE FROM producttag WHERE productid=%s" % conn.literal(productid)
 			sqlcommands.append(sql)
-			cursor.execute(sql)
+			if (not data["dryrun"]) :
+				cursor.execute(sql)
 
 			sql = ""
 			for tag in data["tags"] :
@@ -101,12 +109,14 @@ try:
 				sql += "(%s, %s)" % (conn.literal(productid), conn.literal(tag))
 			sql = "INSERT INTO producttag (productid, tagid) VALUES %s" % sql
 			sqlcommands.append(sql)
-			cursor.execute(sql)
-			if (data['dryrun'] == False) :
-				#conn.commit()
-				outputhtml = 'Successful'
+			if (not data["dryrun"]) :
+				cursor.execute(sql)
+
+			if (data['dryrun']) :
+				outputhtml = "Dry run"
 			else :
-				outputhtml = sqlcommands
+				#conn.commit()
+				outputhtml = "Success"
 
 		elif data["type"] == "addMultiple" :
 			tagids = []
@@ -140,49 +150,61 @@ try:
 			for product in products :
 				sql = "INSERT INTO products (name, image) VALUES(%s, %s)" % (conn.literal(product[0]), conn.literal(product[-1]))
 				sqlcommands.append(sql)
-				cursor.execute(sql)
-				newid = cursor.lastrowid
+				if (not data["dryrun"]) :
+					cursor.execute(sql)
+					newid = cursor.lastrowid
+				else :
+					newid = "1234"
 				
 				vendors = product[vendorloc]
 				for i in range(len(vendors)) :
 					sql = "INSERT INTO links (productid, vendor, link) VALUES (%s, %s, %s)" % (conn.literal(newid), conn.literal(vendors[i]), conn.literal(product[len(tagids)+i+1]))
 					sqlcommands.append(sql)
-					cursor.execute(sql)
+					if (not data["dryrun"]) :
+						cursor.execute(sql)
 
 				for i in range(1,len(tagids)+1) :
 					for tag in product[i] :
 						if tag not in tags[tagids[i-1]] :
 							sql = "INSERT INTO tags (categoryid, value) VALUES (%s, %s)" % (conn.literal(tagids[i-1]), conn.literal(tag))
 							sqlcommands.append(sql)
-							cursor.execute(sql)
-							tagvalue = cursor.lastrowid
+							if (not data["dryrun"]) :
+								cursor.execute(sql)
+								tagvalue = cursor.lastrowid
+							else :
+								tagvalue = "12345"
 							tags[tagids[i-1]][tag] = tagvalue
 						else :
 							tagvalue = tags[tagids[i-1]][tag]
 						sql = "INSERT INTO producttag (productid, tagid) VALUES (%s, %s)" % (conn.literal(newid), conn.literal(tagvalue))
 						sqlcommands.append(sql)
-						cursor.execute(sql)
-			if (data['dryrun'] == False) :
+						if (not data["dryrun"]) :
+							cursor.execute(sql)
+			if (data['dryrun']) :
+				outputhtml = "Dry run"
+			else :
 				#conn.commit()
 				outputhtml = 'Successful'
-			else :
-				outputhtml = sqlcommands
 		elif data["type"] == "newGroup" :
 			sql = "INSERT INTO groups (value, description, image, pathname, spreadsheet) VALUES(%s, %s, %s, %s, %s)" % (conn.literal(data["name"]),conn.literal(data["description"]),conn.literal(data["imageFolder"]),conn.literal(data["imageFilename"]),conn.literal(data["spreadsheet"]))
 			sqlcommands.append(sql)
-			cursor.execute(sql)
-			newid = cursor.lastrowid
+			if (not data["dryrun"]) :
+				cursor.execute(sql)
+				newid = cursor.lastrowid
+			else :
+				newid = "123"
 			
 			categories = re.split(" ?\t ?", data["categories"])
 			for index, category in enumerate(categories) :
 				sql = "INSERT INTO categories (groupid, value, priority) VALUES(%s, %s, %s)" % (conn.literal(newid), conn.literal(category), index+1)
 				sqlcommands.append(sql)
-				cursor.execute(sql)
-			if (data['dryrun'] == False) :
+				if (not data["dryrun"]) :
+					cursor.execute(sql)
+			if (data['dryrun']) :
+				outputhtml = "dry run"
+			else :
 				#conn.commit()
 				outputhtml = 'Successful'
-			else :
-				outputhtml = sqlcommands
 		elif data["type"] == "editGroup" :
 			sql = "UPDATE groups SET value = %s, description = %s, image = %s, pathname = %s, spreadsheet = %s WHERE groupid = %s" % (conn.literal(data["name"]),conn.literal(data["description"]),conn.literal(data["imageFolder"]),conn.literal(data["imageFilename"]),conn.literal(data["spreadsheet"]), conn.literal(data["groupid"]))
 			sqlcommands.append(sql)
